c30d8577becfa8aefba2d27dd421faf1001161e4
JazzAfk <77739604+Jazzafk@users.noreply.github.com>
2022-03-08 11:42:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var startBtn = document.getElementById('start')
 var stopBtn = document.getElementById('stop')
 var start2 = document.getElementById('start2')
-
+num = 1;
 
 function sound(frequency, time){
 audioCtx = new(window.AudioContext || window.webkitAudioContext)();
@@ -21,13 +21,29 @@ var timeout = setTimeout(()=>{
 
 
 startBtn.addEventListener('click', function(){
-   song()
+   repeateSound()
 })
 
-function song(){
-    sound(500, 2000)
 
-var timed = setTimeout(()=>{
-    sound(700, 2000)
-    },time)
+function repeateSound(){
+var interval = setInterval(function(){
+    sound((notes[num]), 500)
+    num = num+1
+    console.log(num)
+
+if (num==8){
+    clearInterval(interval)}
+}, 500*num);
+
+}
+
+var notes = {
+    1: 220,
+    2: 349.228,
+    3: 261.626,
+    4: 261.626,
+    5: 220,
+    6: 349.228,
+    7: 261.626,
+    8: 261.626,
 }
